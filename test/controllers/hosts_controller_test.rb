7745d464dd2048da6bd33ad40c55f522034d1988
kaito kaito45 <kaito@MacBook-Air.local>
2022-10-19 01:00:14 -0700

    ホストをする機能にまつわるコントローラーの作成、ルートの定義をするファイルを作成した

---
+++ test/controllers/hosts_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class HostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get hosts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get hosts_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get hosts_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get hosts_edit_url
+    assert_response :success
+  end
+end
