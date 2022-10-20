8b90e9c48ea861b85fc1619403afb605b59a8642
kaito kaito45 <kaito@MacBook-Air.local>
2022-10-20 03:48:34 -0700

    それぞれのアクションの内容を追加

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,5 +1,6 @@
 class HostsController < ApplicationController
   def index
+    @hosts = Host.all
   end
 
   def show
@@ -9,6 +10,17 @@ class HostsController < ApplicationController
     @host = Host.new
   end
 
+  def create
+    host = Host.new(host_params)
+    host.save!
+    redirect_to hosts_url, notice: "ホストとして物件を登録することができました。"
+  end
+
   def edit
+  end
+
+  private
+  def host_params
+    params.require(:host).permit(:room_name,:room_introduction,:room_charge,:room_address)
   end
 end
