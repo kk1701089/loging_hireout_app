228604e0d5c7b87f3ab96b2484c7f0b35ce69c8f
kaito kaito45 <kaito@MacBook-Air.local>
2022-10-30 03:08:23 -0700

    トップページを表示するルーティングを変更。roomsコントローラーに由来するルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-resources :hosts
+  # トップページを表示するルーティング
+  root 'rooms#index'
+  
+  get 'rooms/index'
+  get 'rooms/search'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/edit'
+
+  # hostsに関するルーティングを一括指定
+  resources :hosts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # トップページを表示するルーティング
-  root 'tops#search'
   get 'tops/search', to: 'tops#search'
   # Defines the root path route ("/")
   # root "articles#index"
