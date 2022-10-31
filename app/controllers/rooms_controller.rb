9a09c4e415e83a1e20a90ef94e167ce8db851cee
kaito kaito45 <kaito@MacBook-Air.local>
2022-10-31 00:47:41 -0700

    searchアクションを追加。各アクションが動作する前にsearchアクションを動作するようにした

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,15 @@
 class RoomsController < ApplicationController
+# 各アクションが動作する前に
+  before_action :search
+
   def index
+    
+  end
+
+  def search
     @q = Host.ransack(params[:q])
     @rooms = @q.result(distinct: true)
-    binding.pry
+    # binding.pry
   end
 
   def show
