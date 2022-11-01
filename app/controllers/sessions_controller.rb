64f29dd3ab4ae8b14f3281df710b44f6d29e6cc6
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 03:52:08 -0700

    ログイン機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # 送られてきたメールアドレスでユーザーを検索する
+    user = User.find_by(email: session_params[:email])
+
+    # ユーザーが見つかった場合は、送られてきたパスワードをauthenticateメソッドで認証可能か判断する
+    if user&.authenticate(session_params[:password])
+      # 認証に成功した場合ユーザーidをセッションに格納
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+  # 送られてきたパラメーターを安全化
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+
 end
