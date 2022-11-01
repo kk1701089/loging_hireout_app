a9122dcf3686aabc8490c89afb54c11108ee4d98
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 04:04:57 -0700

    ユーザー登録するための機能をちゅするための機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    # privateメソッドにより安全化されたuserパラメーターをuser_paramsメソッドで取得して、Userオブジェクトを作成する
+    @user = User.new(user_params)
+    binding.pry
+    # save!メソッドでデータベースに登録
+    @user.save!
+    # トップ画面に遷移
+    redirect_to root_path,notice: "登録に成功しました"
+  end
+
   def edit
+  end
+
+  private
+  # user_paramsメソッドで指定したカラム名以外の登録をできなくする
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
