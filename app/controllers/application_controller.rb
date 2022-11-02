85a934c7c022a55580a36884f89b85ff031b4c65
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 23:30:11 -0700

    すべてのコントローラーやビューから簡単にsesssionidを取得できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,13 @@ class ApplicationController < ActionController::Base
 	private
 
 	def current_user
+		# ||=は左辺がnilでなければ右辺を代入するという意味
+		# if session[:user_id]
+		# @current_user ||= User.find_by(id: session[:user_id])
+		# end と同じ
+		# ログインしていた場合、Userモデルから
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+		# binding.pry
 	end
 
 end
