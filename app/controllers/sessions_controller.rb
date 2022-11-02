67eea40a468b83a51d38dafa39f57cddd42b6292
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 23:36:29 -0700

    コメントを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # 送られてきたメールアドレスでユーザーを検索する
+    # 送られてきたメールアドレスで、Userモデルのemailカラムからユーザーを検索する
+    # 該当したユーザーのレコードをuser変数に格納
     user = User.find_by(email: session_params[:email])
 
     # ユーザーが見つかった場合は、送られてきたパスワードをauthenticateメソッドで認証可能か判断する
+    # &.はuser（レシーバ）がnilの場合でもエラーが出ないようにするメソッド
     if user&.authenticate(session_params[:password])
       # 認証に成功した場合ユーザーidをセッションに格納
       session[:user_id] = user.id
+      binding.pry
       redirect_to root_path, notice: 'ログインしました'
     else
-      render :new
+      render :new, notice: 'ログインに失敗しました'
     end
+  end
+
+  def destroy
+    reset_session
+    binding.pry
+    redirect_to root_url, notice: 'ログアウトしました',status: :see_other
   end
 
   private
