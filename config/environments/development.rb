2fc07d5b839251a08d07c413d0fadae59b69e7a2
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 17:03:31 -0700

    一日ごとにログを出力できるようにした

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # 一日ごとにログを記録する
+  config.logger = Logger.new('log/development.log','daily')
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
