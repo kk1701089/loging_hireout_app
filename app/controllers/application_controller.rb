ca9025c102bf83f9483595d192cc605465a906a4
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-01 17:00:39 -0700

    どのコントローラーファイル、ビューファイルからでも利用できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	# ヘルパーメソッド指定をすることですべてのビューから使用できるようにする。
+  helper_method :current_user
+
+	private
+
+	def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+	end
+
 end
