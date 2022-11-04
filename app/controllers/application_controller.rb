b2e83183479ba9e389bdd884575506b48617cee7
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-04 05:51:08 -0700

    インデント調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-	# ヘルパーメソッド指定をすることですべてのビューから使用できるようにする。
-  helper_method :current_user
+# ヘルパーメソッド指定をすることですべてのビューから使用できるようにする。
+helper_method :current_user
 
-	private
+  private
 
-	def current_user
-		# ||=は左辺がnilでなければ右辺を代入するという意味
-		# if session[:user_id]
-		# @current_user ||= User.find_by(id: session[:user_id])
-		# end と同じ
-		# ログインしていた場合、Userモデルから
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-		# binding.pry
-	end
+  def current_user
+  # ||=は左辺がnilでなければ右辺を代入するという意味
+  # if session[:user_id]
+  # @current_user ||= User.find_by(id: session[:user_id])
+  # end と同じ
+  # ログインしていた場合、Userモデルから
+  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # binding.pry
+  end
 
 end
