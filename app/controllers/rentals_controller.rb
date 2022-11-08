8a0bf119b4ffa43c0b74e7051b7d9cac9c2de43f
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-08 03:57:46 -0800

    宿泊予約フォームからデータを取得予約フォームからデータを取得するフォームからデータを取得するためデータを取得するためにconfirmアクションを追加

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,7 +3,17 @@ class RentalsController < ApplicationController
   end
 
   def new
-    @rental = Rental.new
+    @rentals = Rental.new
+    binding.pry
+  end
+
+  def confirm
+    binding.pry
+    @rentals = Rental.new(rental_params)
+    binding.pry
+    # valid?はエラーがあればfalseを返す
+    # unlessはfalseの時に実行される
+    render :new unless @rentals.valid?
   end
 
   def show
@@ -12,4 +22,10 @@ class RentalsController < ApplicationController
 
   def edit
   end
+
+  private
+  def rental_params
+    params.require(:rental).permit(:rent_room_name, :rent_room_intro,:rent_start_day, :rent_end_day, :rent_room_charge, :people)
+  end
+
 end
