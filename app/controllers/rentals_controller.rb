35753f9e3041003f06fdb301de63de0dc05194a8
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-14 20:46:26 +0900

    フォームで誤ったデータを入力し誤ったデータを入力しようとしデータを入力しようとすると入力前の画面に戻る機能前の画面に戻る機能を追加画面に戻る機能を追加し戻る機能を追加した

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -9,14 +9,28 @@ class RentalsController < ApplicationController
 
   def confirm
     @rentals = Rental.new(rental_params)
-    # binding.pry
+    @room = Host.find(@rentals[:id])
+    binding.pry
     # valid?はエラーがあればfalseを返す
-    # unlessはfalseの時に実行される
-    render :new unless @rentals.valid?
+    unless @rentals.valid?
+      binding.pry
+      # 入力前の画面に戻す
+      render "rooms/show"
+    end
+  end
+
+  def create
+    @rentals = Rental.new(rental_params)
+    @room = Host.find(@rentals[:id])
+    if @rentals.save
+      binding.pry
+      redirect_to @rentals, notice: "予約が完了しました。"
+    else
+      render "rooms/show"
+    end
   end
 
   def show
-    @rental = Host.find(params[:id])
   end
 
   def edit
@@ -24,7 +38,7 @@ class RentalsController < ApplicationController
 
   private
   def rental_params
-    params.require(:rental).permit(:rent_room_name, :rent_room_intro,:rent_start_day, :rent_end_day, :rent_room_charge, :people)
+    params.require(:rental).permit(:id,:rent_room_name, :rent_room_intro,:rent_start_day, :rent_end_day, :rent_room_charge, :people)
   end
 
 end
