b2aca67ef22f5fcc78530a97207cec134a2276e0
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-15 13:15:55 +0900

    アプリケーションコントローラーにメソッドを書くことで、すべてのコントローラーやビューで使えるよコントローラーやビューで使えるようにしているビューで使えるようにしている

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-# ヘルパーメソッド指定をすることですべてのビューから使用できるようにする。
-helper_method :current_user
+  # ヘルパーメソッドをすべてのページから使えるようにする
+  include SessionsHelper
+  # ヘルパーメソッド指定をすることですべてのビューから使用できるようにする。
+  helper_method :current_user
 
   private
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 
   def current_user
   # ||=は左辺がnilでなければ右辺を代入するという意味
@@ -13,5 +22,4 @@ helper_method :current_user
   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   # binding.pry
   end
-
 end
