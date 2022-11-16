6e6d12793da486c819fe61147f6295cec944b032
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-16 21:37:17 +0900

    ログインに失敗した時の処理失敗した時の処理を追加した時の処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     # binding.pry
     # save!メソッドでデータベースに登録
-    @user.save!
-    # トップ画面に遷移
-    redirect_to root_path,notice: "登録に成功しました"
+    if @user.save
+      # トップ画面に遷移
+      redirect_to root_path,notice: "登録に成功しました"
+    else
+      render :new
+    end
   end
 
   def edit
