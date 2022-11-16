0b803873f1f94622a317ef686b11f7863b058880
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-16 21:49:10 +0900

    user_idを紐づける設定を追加紐づける設定を追加しづける設定を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_044413) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_110459) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_044413) do
     t.string "room_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_hosts_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
@@ -57,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_044413) do
     t.integer "rent_room_charge"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
   create_table "user_profiles", force: :cascade do |t|
