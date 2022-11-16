8a756d5044967b99a5bc77417dc3404acabdd4de
kaito kaito45 <kaito@MacBook-Air.local>
2022-11-16 21:38:52 +0900

    コメントの通り

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  # hostモデルと結びつける
+  has_many :hosts
+  
+  # rentalモデルと結びつける
+  has_many :rentals
+  
   validates :name, presence:true
   validates :email,presence:true,uniqueness: true
 end
